7209f20eb07f2961dd98d902fb0ac0b99540c56f
mdaviscph <mdaviscph@gmail.com>
2015-08-03 10:08:32 -0700

    CodeFellows iOS Development Accelerator week 1

---
+++ TwitterApp/Tweet.swift
@@ -0,0 +1,16 @@
+//
+//  Tweet.swift
+//  TwitterApp
+//
+//  Created by mike davis on 8/3/15.
+//  Copyright (c) 2015 mike davis. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+  var text: String
+  var username: String
+  var id: String
+  var profileImageURL: String
+}
